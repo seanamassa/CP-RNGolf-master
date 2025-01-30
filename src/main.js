f2064da9dcd811cb30a691449c82eab53b39857e
Sean Massa <samassa@ucsc.edu>
2025-01-29 18:31:43 -0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,24 @@
+// Code Practice: RNGolf
+// Name: Sean Massa
+// Date: 1/29/2025
+
+'use strict'
+
+//const { Physics } = require("phaser")
+
+let config = {
+    type: Phaser.AUTO,
+    width: 640,
+    height: 960,
+    physics:{
+        default: 'arcade',
+        arcade: {
+            debug: true
+        }
+    },
+    scene: [ Play ]
+}
+
+let game = new Phaser.Game(config)
+
+let { width, height } = game.config
